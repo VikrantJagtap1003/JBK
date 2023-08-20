a246e2d55a8fdb4baddb77d3f89d4c3064566270
VikrantJagtap1003 <122735165+VikrantJagtap1003@users.noreply.github.com>
2023-08-20 10:30:35 +0530

    Add files via upload

---
+++ Assignments/StudentInformation/Main.java
@@ -0,0 +1,12 @@
+package ClassCodes.StudentInformation;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s=new Student();
+        System.out.println(s);
+        Student s1=new Student("vikrant",22);
+        System.out.println(s1);
+        Student s2=new Student();
+        System.out.println(s2);
+    }
+}
